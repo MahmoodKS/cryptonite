58a1d3130a4100937c6ddedc7e02abc23853ae0d
MahmoodAbuSamaha <abusamaha@freightos.com>
2020-02-25 11:56:50 +0200

    Responsive view v1

--- utils.js
+++ utils.js
@@ -1,10 +1,16 @@
 const addCoinsToDom = (coins) => {
+    const $row = $('<div>', { class: 'row' });
+
     $.each(coins, (i, coin) => {
         const { id, symbol, name } = coin;// destructure
-        const $coin = $('<div>', { class: SELECTORS.CURRENCY, id });
-
+        const $coin = $('<div>', {
+            id,
+            class: `${SELECTORS.CURRENCY.replace('.', '')} col-xs-12 col-sm-6 col-md-4 col-lg-3`,
+        });
+        
         $coin.text(symbol + name);
-        $(SELECTORS.CURRENCIES).append($coin);
+        $row.append($coin);
     });
 
+    $(SELECTORS.CURRENCIES).append($row);
 };
